6247d3de93e965173e8fac3341242cea2b7be8b2
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-01-07 15:39:29 +0500

    Create first.py

---
+++ Python/first.py
@@ -0,0 +1,60 @@
+
+# print(dir(__builtins__))
+
+# name = input("Enter your name: ")
+# age = input("Emter your age: ")
+# country = input("where are you form: ")
+# print(name, age, country)
+
+# name = input("Enter your name: ")
+# print(name)
+
+# def myName(name):
+#     print(name)
+#     myName('Dovudbek')
+
+
+# myName('Dovudbek')
+
+# import datetime
+
+
+# print(datetime.MAXYEAR)
+
+# def print_name(name):
+#     print(name)
+
+
+# print_name('Dovudbek')
+
+# print_name = 15
+
+# print_name('Dovudbek')
+# my_number = 'Dovudbek'
+# print(id(my_number))
+# print(type(my_number))
+
+
+# info_msg = '''You are
+# learning the easiest
+# programming languagePython !'''
+# print(info_msg)
+
+# name = 'Dovudbek k '
+# print(len(name))
+# print(name[5:])
+# print(name.replace('Dovudbek', 'alo'))
+# print(name.count(' k'))
+# user = input("sonni kiriting: ")
+# num = int(user)
+# print(num)
+
+
+# base = 5
+# power = 3
+# print(pow(base, power))
+
+# price = 5.16
+# int_price = float(price)
+
+# print(int_price)
