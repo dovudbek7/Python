d62ba902a2f68173c6816193fcf2fafd906f5eee
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-01-13 13:42:00 +0500

    Create lesson2.py

---
+++ It/lesson2.py
@@ -0,0 +1,160 @@
+# s = 'python'
+# print('on' in s)
+# print('kdinf' in s)
+# print('alfa' not in s)
+
+
+# num = 0
+# num += 1
+# num += 2
+# print(num)
+
+# num = 0
+# num -= 1
+# num -= 2
+# print(num)
+
+# num = 0
+# num *= 2
+# num *= 2
+# print(num)
+
+
+# num = 10
+# num /= 2
+# # num //= 2
+# print(num)
+
+
+# x = (5+10)*3/2
+
+# logical operators
+# print(10 == 100)  # False
+# print(10 == 10)  # True
+# print(10 != 10)  # False
+# print(10 != 10.0)  # False
+# print(3 > 2)  # True
+# print(3 < 4)  # True
+# print(3 <= 4)  # True
+
+# y = [1, 5]
+# x = y
+# print(x is y)  # True
+
+
+# a = 'a'
+# b = 'b'
+# print(a is b)  # False
+
+# print(1 > 0 and 3 < 5)  # True
+# print(1 > 0 and 8 < 5)  # False
+# print(1 > 0 and 8 < 5 and 8 >= 30)  # False
+
+# print(1 > 0 or 1 == 0)  # True
+# print(1 < 0 or 1 == 0)  # Flase
+
+# in, is , npt, not in , is not, and , or
+
+# print(not True)  # False
+# print(not False)  # True
+
+# print(not 0)  # True
+# print(not ' ')  # False
+
+
+# if 1 > 0:
+#     print('True')
+# elif 1 < 0:
+#     print("elif")
+# else:
+#     print('else')
+
+# user = input()
+# if user.isalpha():
+#     if 'olma' is user:
+#         print('olma topildi !')
+#     else:
+#         print('olma yoq')
+# else:
+#     print('Iltimos matn kiriting !')
+
+
+# a = 5
+# print(a * 4)
+
+
+# a = 5
+# print(a ** 2)
+
+# a = 5
+# b = 2
+# print(a * b)
+# print(f"P = {2*(a + b)}")
+
+# d = 5
+# p = 3.14
+# print(f"D = {p * d}")
+
+# a = 5
+# print(a ** 3)
+# print(f"{6 * (a ** 2)}")
+
+# a, b, c = 5, 2, 3
+# print(f"V = {a * b * c}")
+# print(f"S = {2 * (a * b + b *c + a *c)}")
+
+
+# r = 5
+# p = 3.14
+# print(f"L = {2*p*r}")
+# print(f"S = {p * (r **2)}")
+
+
+# a, b = 2, 5
+# print((a + b) / 2)
+
+# a, b = 2, 6
+# print((a + b) ** 0.5)
+
+
+# a, b = 5, 2
+# print(a+b,a*b,a**2,b**2)
+
+
+# Task 1
+# a = 2
+# b = 3
+# if a % 2 != 0 and b % 2 != 0:
+#     print(a+b)
+# elif a % 2 != 0 or b % 2 != 0:
+#     print(a*b)
+# else:
+#     print(0)
+
+# Task 2
+# a, b, c = 1, 5, 6
+# print(max(a, b, c))
+
+
+# people_1 = input('people1  ')
+# people_2 = input('people2  ')
+# people_3 = input('people3  ')
+
+# man = 0
+# women = 0
+# if people_1.endswith('v'):
+#     man += 1
+# else:
+#     women += 1
+# if people_2.endswith('v'):
+#     man += 1
+# else:
+#     women += 1
+# if people_3.endswith('v'):
+#     man += 1
+# else:
+#     women += 1
+
+
+# print(man)
+# print(women)
