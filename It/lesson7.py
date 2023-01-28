4ffe9de4510095fa3c4548357a93986436d20823
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-01-28 13:13:11 +0500

    new

---
+++ It/lesson7.py
@@ -0,0 +1,70 @@
+# from functools import reduce
+
+# arr = list(range(6))
+
+
+# def func(x, y):
+#     return x + y
+
+
+# r = reduce(func, arr)
+# print(r)
+
+
+# arr = [1, 2]
+# arr.insert(4, 4)
+# print(len(arr))
+# print((arr))
+# arr[:0] = [-2, -1, 0]
+# print(arr)
+
+
+# import random
+# from faker import Faker
+
+# fake = Faker()
+
+# users = {}
+
+# for i in range(10):
+#     name = fake.name()
+#     temp = {
+#         f'{name.split(" ")[0].lower()}': random.randint(18, 30)
+#     }
+#     users.update(temp)
+
+# print(users)
+
+# import random
+
+# Task 1
+# for i in range(5):
+#     q = random
+#     print(q)
+
+
+# arr = [True,1,24,52,'strri']
+
+# stri = []
+# inti = []
+# boo = []
+# for i in arr:
+#     if type(i) == str:
+#         stri.append(i)
+#     if type(i) == int:
+#         inti.append(i)
+#     if type(i) == bool:
+#         boo.append(i)
+        
+# print(str)
+# print(str)
+# print(boo)
+
+
+# user = int(input('num = '))
+# e = user / 5
+# p = round(e)
+# result = f'кв {user}, подезд{p}, этаж{e} '
+# print(result)
+
+d = {}
---
+++ It/lesson8.py
@@ -0,0 +1,55 @@
+d = {}
+d['name'] = 'John Doe'
+d['age'] = 20
+
+# for item in d.keys():
+#     print(item)
+# for item in d.items():
+#     print(item)
+# for item in d.values():
+#     print(item)
+
+# d.pop("age")
+# print(d)
+# print(d.popitem())
+# print(d.clear())
+# d.update({'salary': 300})
+# print(d)
+
+# keys = "abc"
+# values = [1, 2, 3, 4]
+# d = {k: v for (k, v) in zip(keys, values)}
+
+# print(d)
+
+# Task 4
+# import random
+# from faker import Faker
+# fake = Faker()
+# users = {}
+# for i in range(30):
+#     name = fake.name()
+#     f_name = {
+#         f'{name.split(" ")[0].lower()}': random.randint(10,50)
+#     }
+#     users.update(f_name)
+# print(users)
+
+# Task 6
+# for i in range(1):
+#     user = input('write = ')
+#     if len(user) > 3 and len(user) <=10:
+#         arr = user.split('.')
+#         # print(arr)
+#         if arr[0] > '0' and arr[0] <'12':
+#             if arr[1] > '0' and arr[1] < '31':
+#                 if arr[2] > '1970' and arr[2] < '2023':
+#                     print(arr)
+#         else:
+#             print(False)
+
+# Task 8
+user = input('..')
+
+
+print(f'{int(user[0:2])-15}')
