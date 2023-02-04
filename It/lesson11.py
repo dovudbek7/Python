57e1abd56e3e6a9d35a57746dd0353cd3d0021b3
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-02-04 13:07:20 +0500

    new

--- It/lesson11.py
+++ It/lesson11.py
@@ -1,28 +1,28 @@
-# pip -m pip install --upgrade pip
-# import colorama
-# print()
+# # pip -m pip install --upgrade pip
+# # import colorama
+# # print()
 
 
 
 
-    # all_links = persering.findAll('a')
-    # for i in all_links:
-    #     print('https://pyblog.uz/'+link['href'])
-    #     print(link.text)
+#     # all_links = persering.findAll('a')
+#     # for i in all_links:
+#     #     print('https://pyblog.uz/'+link['href'])
+#     #     print(link.text)
     
     
-import requests
-import wget
-from bs4 import BeautifulSoup as bs
-url = 'https://pyblog.uz'
+# import requests
+# import wget
+# from bs4 import BeautifulSoup as bs
+# url = 'https://pyblog.uz'
 
-page = requests.get(url=url)
-print(page.status_code)
+# page = requests.get(url=url)
+# print(page.status_code)
 
-if page.status_code == 200:
-    persering = bs(page.text, 'html.parser')
-    images  = persering.findAll("img")
-    for i in images:
-        wget.download('https://pyblog.uz'+i["src"])
-else:
-    print('Not Founded')
+# if page.status_code == 200:
+#     persering = bs(page.text, 'html.parser')
+#     images  = persering.findAll("img")
+#     for i in images:
+#         wget.download('https://pyblog.uz'+i["src"])
+# else:
+#     print('Not Founded')
