037488664b0c2c3d22139631d7fd02abcd52cd6a
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-02-13 16:57:47 +0500

    new

---
+++ It/lesson14.py
@@ -0,0 +1,29 @@
+# class Bank:
+#     cash = 1300
+#     web_money = 560
+    
+#     _cash = 1500  # priate
+#     __cash = 600 # protect
+#     def pay(self, payment_method, money):
+#         if payment_method == 'cash':
+#             self.cash - money
+#             print(f'Bank cash : {self.cash}')
+#         elif payment_method == 'web':
+#             self.web_money = money
+#             print(f'Bank web money : {self.web_money}')
+#         else:
+#             print('Payment method is not support!')
+#     def chack_balance(self):
+#             print(f'Bank cash : {self.cash}')
+#             print(f'Bank cash : {self.web_money}')
+
+# b  = Bank()
+# print(b.cash)
+# print(b._cash)
+# print(b._b_cash)
+
+
+
+
+
+
