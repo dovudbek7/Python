f465970f91ba78b129f6e5e4802f5a91a51ed400
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-02-19 15:31:51 +0500

    new

---
+++ It/lesson16.py
@@ -0,0 +1,52 @@
+# user = input('Bo\'yi : ')
+# user = input('eni : ')
+
+
+# k1 = input('kengru 1 : ')
+# k2 = input('kengru 2 : ')
+
+# while True:
+    
+#     if x == y:
+#         print('stop')
+#         break
+        
+# # if k1 == k2:
+#     print('aaa')
+
+
+user = input('sonni kiriting : ')
+arr = []
+sss = []
+for i in user:
+    if i[0].isdigit():
+        arr.append(i)
+
+# a = int(arr[0]) + int(arr[1]) + int(arr[2])
+# b = a //3
+print(user.split())
+
+
+        
+
+.
+.
+
+
+
+
+
+
+
+
+
+
+
+
+# if len(user) < 9 :
+#     print('9ta belgi yozing!')
+# elif len(user) == 9:
+#     print('ok')
+    
+# else:
+#     print('9 Ta harfdan belgidan!')    
