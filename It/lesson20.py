253f0624e2dd507d3c65eb0c68a2b1a7df2487ed
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-01 15:41:19 +0500

    add

--- It/lesson20.py
+++ It/lesson20.py
@@ -1,2 +1,31 @@
-for k in range(3,7):
-    print(f'{k*str(k)}')
+# for k in range(3,7):
+#     print(f'{k*str(k)}')
+
+# for numbers in range(1,10):
+#     break
+
+# for i in range(1,11):
+#     print(f'{numbers}+{i}={numbers+i}')
+# for numbers in range(2,10):
+#     break
+
+# for i in range(1,11):
+#     print(f'{numbers}+{i}={numbers+i}')
+# for numbers in range(9,10):
+#     break
+
+# for i in range(1,11):
+#     print(f'{numbers}+{i}={numbers+i}')
+
+
+# task a 
+
+# for i in range(1,10):
+#     for x in range(1,10):
+#         print(f'{i} + {x} = {i+x}')
+    
+# task b
+
+# for i in range(1,10):
+#     for x in range(1,10):
+#         print(f'{x} + {i} = {i+x}')
