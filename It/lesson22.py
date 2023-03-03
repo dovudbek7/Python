b57e871283619a58260051d7799e06ddbae22e37
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-03 17:48:30 +0500

    add

--- It/lesson22.py
+++ It/lesson22.py
@@ -1,26 +1,40 @@
 import random
 from faker import Faker
 
+# f = Faker()
+# passed = []
+# nop = []
+# for i in range(10):
+#     students = f.name()
+#     all = []
+#     for b in range(3):
+#         point = random.randint(10, 33)
+#         all.append(point)
+#     s = sum(all)
+#     d = {students : s}
+#     print(d)
+#     if s >= 75:
+#         x = list(d.keys())[list(d.values()).index(s)]
+#         a = f'{x} : {s}'
+#         passed.append(a)
+#     else:
+#         m = list(d.keys())[list(d.values()).index(s)]
+#         b = f'{m} : {s}'
+#         nop.append(b)
+
+# print(passed)
+# print(nop)
+
 f = Faker()
-passed = []
-nop = []
-for i in range(10):
+for i in range(30):
     students = f.name()
-    all = []
-    for b in range(3):
-        point = random.randint(10, 33)
-        all.append(point)
-    s = sum(all)
-    d = {students : s}
-    print(d)
-    if s >= 75:
-        x = list(d.keys())[list(d.values()).index(s)]
-        a = f'{x} : {s}'
-        passed.append(a)
-    else:
-        m = list(d.keys())[list(d.values()).index(s)]
-        b = f'{m} : {s}'
-        nop.append(b)
+    res = []
+    for b in range(4):
+        point = random.randint(2, 5)
+        res.append(point)
+    s = sum(res)
+    if i % 2 == 0:
+        print(students)
+
     
-print(passed)
-print(nop)
+
